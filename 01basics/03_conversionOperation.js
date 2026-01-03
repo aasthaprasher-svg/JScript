70054260af0646ce06d677275baf9ef02ef55e9a
aasthaprasher-svg <aasthaprasher@gmail.com>
2026-01-03 10:38:40 +0000

    operations

--- 01basics/03_conversionOperation.js
+++ 01basics/03_conversionOperation.js
@@ -24,6 +24,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 43
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*******************Operations******************
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2-2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Aastha"
+
+let str3 = str1 + " " + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+"")
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
 
